9ced9ecb9d6c0f89f69366d17f8525f5c8c3b4c6
sajadgilga <majidstic@yahoo.com>
2021-03-26 22:22:51 +0430

    added getters and setters to application

--- src/main/java/Rectangle.java
+++ src/main/java/Rectangle.java
@@ -10,6 +10,21 @@ public class Rectangle {
         this.height = height;
     }
 
+    public int getWidth() {
+        return width;
+    }
+
+    public void setWidth(int width) {
+        this.width = width;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
 
     public long computeArea() {
         return (long) width * height;
