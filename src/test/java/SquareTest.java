6786618ea449be94ba106f334c7f8580b1f25d24
svafaiet <svafaiet@gmail.com>
2021-03-26 23:30:50 +0430

    Part3: Fix squarte tests

--- src/test/java/SquareTest.java
+++ src/test/java/SquareTest.java
@@ -56,7 +56,7 @@ public class SquareTest {
     public void shouldChangeGetOnSet() {
         try {
             Square sq = new Square(4);
-            sq.getSide(5);
+            sq.setSide(5);
             assertEquals(sq.getSide(), 5);
         } catch (Exception e) {
             fail(e.getMessage());
@@ -64,10 +64,25 @@ public class SquareTest {
     }
 
     @Test
+    public void shouldSetNegativeFail() {
+        Rectangle rect = new Rectangle(1, 3);
+        try {
+            rect.setWidth(-1);
+        } catch (Exception e) {
+            assertEquals(e.getMessage(), "Sides must be positive");
+        }
+        try {
+            rect.setHeight(-1);
+        } catch (Exception e) {
+            assertEquals(e.getMessage(), "Sides must be positive");
+        }
+    }
+
+    @Test
     public void shouldChangeComputeAreaOnSetCorrect() {
         List<Integer> sides = Arrays.asList(1, 5, 12, 10);
         List<Integer> area = Arrays.asList(1, 25, 144, 100);
-        Square sq = new Square(3);
+        Square sq;
         for (int i = 0; i < 4; i++) {
             try {
                 sq = new Square(sides.get(i));
