4ba5302cf5c06b5385062023cf89392eae25413a
sajadgilga <majidstic@yahoo.com>
2021-03-26 22:20:14 +0430

    added test:get correct set correct change area correct to app

--- src/main/java/Rectangle.java
+++ src/main/java/Rectangle.java
@@ -2,13 +2,14 @@ public class Rectangle {
     private int width;
     private int height;
 
-    Rectangle(int width, int height){
+    Rectangle(int width, int height) {
         if (height < 0 || width < 0) {
             throw new IllegalArgumentException("Sides must be positive");
         }
         this.width = width;
         this.height = height;
     }
+
 
     public long computeArea() {
         return (long) width * height;
--- src/test/java/RectangleTest.java
+++ src/test/java/RectangleTest.java
@@ -1,5 +1,6 @@
 import org.junit.Test;
 
+import java.awt.*;
 import java.util.Arrays;
 import java.util.List;
 
@@ -37,7 +38,48 @@ public class RectangleTest {
         for (int i = 0; i < 4; i++) {
             try {
                 Rectangle rect = new Rectangle(side1.get(i), side2.get(i));
-                assertEquals(rect.computeArea(),(long)  area.get(i));
+                assertEquals(rect.computeArea(), (long) area.get(i));
+            } catch (Exception e) {
+                fail(e.getMessage());
+            }
+        }
+    }
+
+    @Test
+    public void shouldGetCorrect() {
+        try {
+            Rectangle rect = new Rectangle(1, 3);
+            assertEquals(rect.getWidth(), 1);
+            assertEquals(rect.getHeight(), 3);
+        } catch (Exception e) {
+            fail(e.getMessage());
+        }
+    }
+
+    @Test
+    public void shouldChangeGetOnSet() {
+        try {
+            Rectangle rect = new Rectangle(4, 7);
+            rect.setWidth(1);
+            rect.setHeight(4);
+            assertEquals(rect.getWidth(), 1);
+            assertEquals(rect.getHeight(), 3);
+        } catch (Exception e) {
+            fail(e.getMessage());
+        }
+    }
+
+    @Test
+    public void shouldChangeComputeAreaOnSetCorrect() {
+        List<Integer> side1 = Arrays.asList(1, 5, 12, 10);
+        List<Integer> side2 = Arrays.asList(2, 8, 11, 10);
+        List<Integer> area = Arrays.asList(2, 40, 132, 100);
+        for (int i = 0; i < 4; i++) {
+            try {
+                Rectangle rect = new Rectangle(1, 3);
+                rect.setWidth(side1.get(i));
+                rect.setHeight(side2.get(i));
+                assertEquals(rect.computeArea(), (long) area.get(i));
             } catch (Exception e) {
                 fail(e.getMessage());
             }
